a442a52100ae22e22fbe5b2d1cfba5696d259707
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2015-09-22 11:48:27 -0400

    added driver class to project

---
+++ src/MethodsAndArrays.java
@@ -0,0 +1,14 @@
+
+public class MethodsAndArrays {
+
+	public static void main(String[] args) {
+		long myLongVariable = 100000L;
+		int myIntVariable;
+
+		
+		myIntVariable = (int) myLongVariable;
+		
+		System.out.println(myIntVariable);
+	}
+
+}
