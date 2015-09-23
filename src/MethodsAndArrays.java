6393f1a238a2a20403b3a00fd609a98be6756a8e
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2015-09-22 20:55:32 -0400

    Added try / catch exception handling to driver class

--- src/MethodsAndArrays.java
+++ src/MethodsAndArrays.java
@@ -7,20 +7,28 @@ public class MethodsAndArrays {
 	public static void main(String[] args) {
 		
 		Game game = new Game(); // instantiates our game object
-
+		
+		
 		
 		Scanner input = new Scanner(System.in);
 		int numDice;
 		int numSides;
 	
+		try {
+			
+			System.out.print("Enter the number of dice: ");
+			numDice = input.nextInt();
+			
+			System.out.print("Enter the number of sides: ");
+			numSides = input.nextInt();
+			
+			System.out.printf("You rolled %d%n",game.rollDice(numDice, numSides));
+			
+		} catch (Exception e) {
+			System.out.println("You entered invalid data!");
+		}
 		
-		System.out.print("Enter the number of dice: ");
-		numDice = input.nextInt();
 		
-		System.out.print("Enter the number of sides: ");
-		numSides = input.nextInt();
-		
-		System.out.printf("You rolled %d%n",game.rollDice(numDice, numSides));
 	}
 	
 	
